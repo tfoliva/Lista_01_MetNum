bbea1abd4f8e37af8cf0455877c4c998fe905679
Tiago Oliva <tiago.oliva.costa@gmail.com>
2023-10-11 16:47:52 -0300

    reformatting

--- fdcoeff.py
+++ fdcoeff.py
@@ -9,6 +9,8 @@ k    - kth derivative order
 xbar - target point to approximate around
 x    - vector of N stencil points
 """
+
+
 def fdcoeffV(k, xbar, x):
     if isinstance(x, list):
         x = np.array(x)
@@ -17,13 +19,14 @@ def fdcoeffV(k, xbar, x):
     A = np.ones((n, n))
     xrow = np.transpose(x - xbar)  # displacements as a row vector.
 
-    for i in range(1, n+1):
-        A[i-1, :] = np.divide(np.power(xrow, (i - 1)), math.factorial(i - 1))
+    for i in range(1, n + 1):
+        A[i - 1, :] = np.divide(np.power(xrow, (i - 1)), math.factorial(i - 1))
 
     b = np.zeros((n, 1))  # b is right hand side,
     b[k] = 1  # so k’th derivative term remains
     c = np.linalg.solve(A, b)  # solve system for coefficients
     return np.transpose(c)  # row vector
+
 
 def u_approx(x, c, f, verbose=True):
     if isinstance(x, list):
@@ -40,16 +43,18 @@ def u_approx(x, c, f, verbose=True):
         print(f"u_k(x): {u_k}\n")
     return u_k
 
+
 def approx_derivative(k, xbar, x, f):
     coeffs = fdcoeffV(k, xbar, x)
     return u_approx(x, coeffs, f, verbose=False)
+
 
 if __name__ == "__main__":
     # example for xbar = 3
     # backward with 2 points
     xbar = 3
     h = 1
-    x = np.array([xbar, xbar -h, xbar - 2*h])
-    k = 1 # first derivative
+    x = np.array([xbar, xbar - h, xbar - 2 * h])
+    k = 1  # first derivative
 
     print(f"The coefficients are {fdcoeffV(k, xbar, x)}")
